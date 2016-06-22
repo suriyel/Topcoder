fa3e84eca9ee11081881f388bd8d06ac809219b5
Shannon <917748823@qq.com>
2016-06-23 00:19:02 +0800

    添加UT

--- SRM156/Problem250/Test/DiskSpaceTest.cs
+++ SRM156/Problem250/Test/DiskSpaceTest.cs
@@ -4,10 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Test
 {
+    [TestFixture]
     public class DiskSpaceTest
     {
+        [Test]
+        public void Test1()
+        {
+            
+        }
     }
 }
